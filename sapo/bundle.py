Fixed major bug concerning sign error with projecting data. Also fixed misimplementation of Bernstein for incomplete polynomials

sapo/bundle.py
@@ -41,10 +41,11 @@ class Bundle:
 
         for ind in range(self.num_direct):
             A[ind] = self.L[ind]
-            A[ind + self.num_direct] = -1 * self.L[ind]
+            A[ind + self.num_direct] = np.negative(self.L[ind])
             b[ind] = self.offu[ind]
             b[ind + self.num_direct] = self.offl[ind]
 
+        #print(A, b)
         return (A,b)
 
     #Canonize the bundle.
@@ -92,11 +93,14 @@ class BundleTransformer:
 
         #get parallelotope P_i
         for row_ind, row in enumerate(bund.T):
+            #print(''.join(['Row: ', str(row)]))
 
             #Calcuate minimum and maximum points of p_i
             p = bund.getParallelotope(row_ind)
             p_min_coord = p.getMinPoint()
             p_max_coord = p.getMaxPoint()
+            
+            #print('Min/Max points for Parall' , row_ind ,'   ', p_min_coord, p_max_coord, '\n')
 
             #Calculate transformation subsitutions
             var_sub = []
@@ -114,27 +118,29 @@ class BundleTransformer:
                 #compute polynomial \Lambda_i \cdot (f(v(x)))
 
                 bound_polyu = [ curr_L[func_ind] * func for func_ind, func in enumerate(self.f) ]
-                bound_polyl = [ -1 * curr_L[func_ind] * func for func_ind, func in enumerate(self.f) ]
-
-                bound_polyu = reduce(add, bound_polyu)
-                bound_polyl = reduce(add, bound_polyl)
-
-                #transform to range over unit box
+                bound_polyu = reduce(add, bound_polyu) #transform to range over unit box
+                
                 transf_bound_polyu = bound_polyu.subs(var_sub)
-                transf_bound_polyl = bound_polyl.subs(var_sub)
-                #print(transf_bound_polyu, transf_bound_polyl)
-
+                #print(''.join(['uPoly: ', str(transf_bound_polyu),'  lPoly: ', str(transf_bound_polyl)]))
                 #Calculate min/max Bernstein coefficients
                 base_convertu = BernsteinBaseConverter(transf_bound_polyu, bund.vars)
-                max_bern_coeffu, min_bern_coeffu = base_convertu.computeBernCoeff()
 
-                base_convertl = BernsteinBaseConverter(transf_bound_polyl, bund.vars)
-                max_bern_coeffl, min_bern_coeffl = base_convertl.computeBernCoeff()
+                max_bern_coeffu, min_bern_coeffu = base_convertu.computeBernCoeff() #Converging example.
+                                                                                    #Diverging at different speeds.
+                                                                                    #Needs more rigorous testing. Understand the logic/algorithm carefully.
+                                                                                    #Max(min_bern_coeffu, max_bern_coeffl) - lower bound
+                                                                                    #Min(max_bern_coeffu, min_bern_coeffl) - upper bound
+                                                                                    #Min/points  are not updating correctly.
 
-                #print(''.join(['Max:', str(max_bern_coeffu),' Min: ', str(max_bern_coeffl), 'for P: ', str(row)]))
+                #base_convertl = BernsteinBaseConverter(transf_bound_polyl, bund.vars)
+                #max_bern_coeffl, min_bern_coeffl = base_convertl.computeBernCoeff()
+                #print(''.join(["Upperbound: ", str((max_bern_coeffu, min_bern_coeffl)), "  Lowerbound:  ", str((min_bern_coeffu, max_bern_coeffl)), '\n' ]))
+
+                #print(''.join(['Max:', str(max_bern_coeffu),' Min: ', str(max_bern_coeffl), 'for P: ', str(row), '\n']))
                 p_new_offu[column] = min(max_bern_coeffu, p_new_offu[column])
-                p_new_offl[column] = min(max_bern_coeffl, p_new_offl[column])
+                p_new_offl[column] = min(-1 * min_bern_coeffu, p_new_offl[column])
 
+        #print(''.join([' p_new_offu: ', str(p_new_offu), ' p_new_offl: ', str(p_new_offl), '\n']))
         trans_bund = Bundle(bund.T, bund.L, p_new_offu, p_new_offl, bund.vars) #Major issues could arise with unused direcitions
-        canon_bund = trans_bund.canonize()
-        return canon_bund
+        #canon_bund = trans_bund.canonize()
+        return trans_bund